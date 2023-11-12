47287bf5eb4ce962d0f2f4ed8af759bf2da439ca
sinminseok12 <sinminseok12@naver.com>
2023-11-12 22:03:48 +0900

    feat(MainController): 프로그램 흐름을 제어할 컨트롤러 클래스 생성

---
+++ src/main/java/christmas/controller/MainController.java
@@ -0,0 +1,18 @@
+package christmas.controller;
+
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class MainController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public MainController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void run() {
+
+    }
+}
