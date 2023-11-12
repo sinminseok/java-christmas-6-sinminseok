cbecc1927afcec95da2e9bc07be0dc54c2e0948f
sinminseok12 <sinminseok12@naver.com>
2023-11-13 01:30:04 +0900

    test(MenuItemTest): 메뉴 아이템과 관련된 기능 테스트 구현

---
+++ src/test/java/christmas/domain/menu/MenuItemTest.java
@@ -0,0 +1,36 @@
+package christmas.domain.menu;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MenuItemTest {
+
+    private MenuItem menuItem;
+
+    @BeforeEach
+    void setInit() {
+        menuItem = new MenuItem(Menu.of("메인", 10000, MenuType.MAIN), 1);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource(value = {"MAIN, true", "APPETIZER, false", "DESERT, false", "DRINKING, false"})
+    void compareMenuType_메서드는_메뉴타입을_비교해_동일성을_판별한다(MenuType menuType, boolean result) {
+        //when
+        boolean sameMenuType = menuItem.compareMenuType(menuType);
+        //then
+        assertThat(sameMenuType).isEqualTo(result);
+    }
+
+    @Test
+    void calculatePrice_메서드는_메뉴의_가격과_수량을_곱한다() {
+        //when
+        Integer price = menuItem.calculatePrice();
+        //then
+        assertThat(price).isEqualTo(20000);
+    }
+}
