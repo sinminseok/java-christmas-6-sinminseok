e33310e56c699943102aa6d94e424895463a7e7f
sinminseok12 <sinminseok12@naver.com>
2023-11-13 01:13:48 +0900

    test(OrderTest): 주문과 관련된 기능 테스트 구현

--- src/test/java/christmas/domain/order/OrderTest.java
+++ src/test/java/christmas/domain/order/OrderTest.java
@@ -1,19 +1,40 @@
 package christmas.domain.order;
 
-import org.assertj.core.api.Assertions;
+import christmas.domain.menu.Menu;
+import christmas.domain.menu.MenuItem;
+import christmas.domain.menu.MenuType;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static christmas.data.DummyData.provideOrderData;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderTest {
+    private static final Menu menu1 = Menu.of("메뉴1", 5000, MenuType.MAIN);
+    private static final MenuItem menu2 = new MenuItem(Menu.of("메뉴2", 5000, MenuType.MAIN), 2);
+    private static final MenuItem menu3 = new MenuItem(Menu.of("메뉴3", 5000, MenuType.MAIN), 3);
 
     @Test
     void getOrderPrice_메서드는_총_주문금액을_구한다() {
         //given
-        Order order = provideOrderData();
+        Order order = new Order(List.of(menu2, menu3));
         //when
         Integer orderPrice = order.getOrderPrice();
         //then
-        Assertions.assertThat(orderPrice).isEqualTo(300000);
+        assertThat(orderPrice).isEqualTo(25000);
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1, false", "2, true"})
+    void canApplyEvent_메서드는_이벤트_적용이_가능한지_판별한다(int menuCount, boolean result) {
+        //given
+        Order order = new Order(List.of(new MenuItem(menu1, menuCount)));
+        //when
+        boolean canApplyEvent = order.canApplyEvent();
+        //then
+        assertThat(canApplyEvent).isEqualTo(result);
+    }
+
 }
