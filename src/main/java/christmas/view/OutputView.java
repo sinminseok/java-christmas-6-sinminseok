6df74feed033bfe80e79fda2917d422d24d9f33f
sinminseok12 <sinminseok12@naver.com>
2023-11-12 22:01:36 +0900

    feat(OutputView): 혜택 미리보기 문구 출력하는 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,9 +1,17 @@
 package christmas.view;
 
+import java.text.MessageFormat;
+import java.time.LocalDate;
+
 public class OutputView {
     private static final String START_PLANNER_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    private static final String EVENT_PREVIEW_MESSAGE = "12월 {0}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
 
     public static void printStartMessage() {
         System.out.println(START_PLANNER_MESSAGE);
+    }
+
+    public static void printEventPreview(LocalDate localDate) {
+        System.out.println(MessageFormat.format(EVENT_PREVIEW_MESSAGE, localDate.getDayOfMonth()));
     }
 }
