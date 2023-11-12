09b951afe8b4ea1179469811518d32d075bd9658
sinminseok12 <sinminseok12@naver.com>
2023-11-12 19:47:07 +0900

    feat(MenuRepository): 제공된 메뉴 정보를 저장하는 기능 구현

---
+++ src/main/java/christmas/repository/MenuRepository.java
@@ -0,0 +1,29 @@
+package christmas.repository;
+
+import christmas.domain.Menu;
+import christmas.domain.MenuType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MenuRepository {
+    private static final List<Menu> menus = new ArrayList<>();
+
+    static {
+        menus.add(Menu.of("양송이수프", 6000, MenuType.APPETIZER));
+        menus.add(Menu.of("타파스", 5500, MenuType.APPETIZER));
+        menus.add(Menu.of("시저샐러드", 8000, MenuType.APPETIZER));
+
+        menus.add(Menu.of("티본스테이크", 55000, MenuType.MAIN));
+        menus.add(Menu.of("바비큐립", 54000, MenuType.MAIN));
+        menus.add(Menu.of("해산물파스타", 35000, MenuType.MAIN));
+        menus.add(Menu.of("크리스마스파스타", 25000, MenuType.MAIN));
+
+        menus.add(Menu.of("초코케이크", 15000, MenuType.DESERT));
+        menus.add(Menu.of("아이스크림", 5000, MenuType.DESERT));
+
+        menus.add(Menu.of("제로콜라", 3000, MenuType.DRINKING));
+        menus.add(Menu.of("레드와인", 6000, MenuType.DRINKING));
+        menus.add(Menu.of("샴페인", 25000, MenuType.DRINKING));
+    }
+}
