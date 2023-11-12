20bed67b4586e53efdffcfd29b718c4a80844569
sinminseok12 <sinminseok12@naver.com>
2023-11-12 21:59:29 +0900

    feat(OutputView): 프로그램 시작 문구를 출력하는 기능 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,9 @@
+package christmas.view;
+
+public class OutputView {
+    private static final String START_PLANNER_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+
+    public static void printStartMessage() {
+        System.out.println(START_PLANNER_MESSAGE);
+    }
+}
