bb1327407ae815c54dbb125ac2afe653876a8ff5
sinminseok12 <sinminseok12@naver.com>
2023-11-13 18:31:24 +0900

    feat(Order): 주문 메뉴가 모두 음료수일 경우를 검증하는 기능 구현

--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -1,5 +1,6 @@
 package christmas.domain.order;
 
+import christmas.domain.menu.MenuType;
 import christmas.domain.menu.OrderMenu;
 import christmas.utils.ErrorMessage;
 
@@ -12,21 +13,28 @@ public class Order {
     private static final Integer MIN_MENU_RANGE = 1;
     private static final Integer MAX_MENU_RANGE = 20;
 
-    private final List<OrderMenu> menuItems;
+    private final List<OrderMenu> orderMenus;
 
-    public Order(List<OrderMenu> menuItems) {
-        validateMenuSize(menuItems);
-        this.menuItems = menuItems;
+    public Order(List<OrderMenu> orderMenus) {
+        validateMenuSize(orderMenus);
+        validateMenuAllDrinking(orderMenus);
+        this.orderMenus = orderMenus;
     }
 
-    private void validateMenuSize(List<OrderMenu> menuItems) {
-        if(menuItems.size() < MIN_MENU_RANGE || menuItems.size() > MAX_MENU_RANGE){
+    private void validateMenuSize(List<OrderMenu> orderMenus) {
+        if(orderMenus.size() < MIN_MENU_RANGE || orderMenus.size() > MAX_MENU_RANGE){
+            throw new ErrorMessage(ORDER_ERROR);
+        }
+    }
+
+    private void validateMenuAllDrinking(List<OrderMenu> orderMenus){
+        if (!orderMenus.stream().allMatch(orderMenu -> orderMenu.compareMenuType(MenuType.DRINKING))) {
             throw new ErrorMessage(ORDER_ERROR);
         }
     }
 
     public Integer getOrderPrice() {
-        return menuItems.stream()
+        return orderMenus.stream()
                 .mapToInt(OrderMenu::calculatePrice)
                 .sum();
     }
@@ -35,7 +43,7 @@ public class Order {
         return getOrderPrice() >= MIN_TOTAL_PRICE;
     }
 
-    public List<OrderMenu> getMenuItems() {
-        return menuItems;
+    public List<OrderMenu> getOrderMenus() {
+        return orderMenus;
     }
 }
