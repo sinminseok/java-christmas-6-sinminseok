9f59ca0489aebaabc5813279ece5d0858699ba6f
sinminseok12 <sinminseok12@naver.com>
2023-11-13 18:25:38 +0900

    feat(Order): 주문 갯수를 검증하는 기능 구현

--- src/main/java/christmas/domain/order/Order.java
+++ src/main/java/christmas/domain/order/Order.java
@@ -1,16 +1,28 @@
 package christmas.domain.order;
 
 import christmas.domain.menu.OrderMenu;
+import christmas.utils.ErrorMessage;
 
 import java.util.List;
 
+import static christmas.utils.ErrorConstants.ORDER_ERROR;
+
 public class Order {
     private static final Integer MIN_TOTAL_PRICE = 10000;
+    private static final Integer MIN_MENU_RANGE = 1;
+    private static final Integer MAX_MENU_RANGE = 20;
 
     private final List<OrderMenu> menuItems;
 
     public Order(List<OrderMenu> menuItems) {
+        validateMenuSize(menuItems);
         this.menuItems = menuItems;
+    }
+
+    private void validateMenuSize(List<OrderMenu> menuItems) {
+        if(menuItems.size() < MIN_MENU_RANGE || menuItems.size() > MAX_MENU_RANGE){
+            throw new ErrorMessage(ORDER_ERROR);
+        }
     }
 
     public Integer getOrderPrice() {
