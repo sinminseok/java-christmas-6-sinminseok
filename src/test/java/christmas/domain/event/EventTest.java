1d3d5d7a15e59c58c0b90f2fde5691c7438c1d65
sinminseok12 <sinminseok12@naver.com>
2023-11-13 23:57:58 +0900

    refactor(EventTest): 이벤트 테스트코드 독립적으로 실행되게 수정

--- src/test/java/christmas/domain/event/EventTest.java
+++ src/test/java/christmas/domain/event/EventTest.java
@@ -4,63 +4,116 @@ import christmas.domain.day.CalendarDay;
 import christmas.domain.event_condition.*;
 import christmas.domain.menu.Menu;
 import christmas.domain.menu.MenuType;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
-import java.util.Arrays;
-import java.util.List;
 
-import static christmas.data.DummyData.provideEventRewardContextData;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EventTest {
+    private Event discountEvent;
+    private Event giftEvent;
+
+    @BeforeEach
+    void setInit() {
+        discountEvent = Event.of("테스트 할인 이벤트", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 25), new TestDiscountEventCondition());
+        giftEvent = Event.of("테스트 증정 이벤트", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 25), new TestGiftEventCondition());
+    }
+
+    @Test
+    void findReward_메서드는_이벤트_보상을_Reward_타입으로_반환한다() {
+        //given
+        EventRewardContext context = provideTestRewardContext();
+        //when
+        Reward reward = discountEvent.findReward(context);
+        //then
+        assertThat(reward.getName()).isEqualTo("테스트 할인 이벤트");
+        assertThat(reward.getDiscountPrice()).isEqualTo(1000);
+    }
+
+    @Test
+    void wrapRewardMenu_메서드는_증정_이벤트_보상을_반환하기_위해_RewardMenu_로_반환한다() {
+        //given
+        EventRewardContext context = provideTestRewardContext();
+        //when
+        RewardMenu rewardMenu = giftEvent.wrapRewardMenu(context);
+        //then
+        assertThat(rewardMenu.getMenuName()).isEqualTo("증정메뉴");
+        assertThat(rewardMenu.getCount()).isEqualTo(1);
+    }
 
     @ParameterizedTest
-    @MethodSource("provideEventData")
-    void canApplyEvent_메서드는_주문과_선택한_날짜를_기반으로_이벤트가_적용가능한지_판별한다(Event event, boolean result) {
+    @CsvSource(value = {"25, true", "26, false"})
+    void isApplicable_메서드는_이벤트_참여_여부를_확인한다(int day, boolean result) {
         //given
-        CalendarDay calendarDay = new CalendarDay(LocalDate.of(2023, 12, 25), true);
-        EventConditionContext eventApplyParameter = new EventConditionContext(150000, calendarDay);
+        EventConditionContext context = provideTestConditionContext(day);
         //when
-        boolean canApplyEvent = event.isApplicable(eventApplyParameter);
+        boolean applicable = discountEvent.isApplicable(context);
         //then
-        assertThat(canApplyEvent).isEqualTo(result);
+        assertThat(applicable).isEqualTo(result);
     }
 
     @Test
-    void findReward_메서드는_이벤트의_혜택을_찾는다() {
-        //given
-        Event event = Event.of("크리스마스 디데이 할인", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 25), new ChristmasEventCondition());
-        Reward resultReward = new Reward("크리스마스 디데이 할인", 3400);
+    void isGiftEvent_메서드는_이벤트가_증정이벤트_인지_판별한다() {
         //when
-        Reward reward = event.findReward(provideEventRewardContextData());
+        boolean result = giftEvent.isGiftEvent();
         //then
-        Assertions.assertThat(reward).isEqualTo(resultReward);
+        assertThat(result).isTrue();
     }
 
     @Test
-    void wrapMenuItemReward_메서드는_이벤트_보상을_MenuItem_타입으로_반환한다() {
-        //given
-        Event event = Event.of("증정 이벤트", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 31), new GiftEventCondition());
-        RewardMenu rewardMenu = new RewardMenu(Menu.of("샴페인", 25000, MenuType.DRINKING), 1);
+    void isDiscountEvent_메서드는_이벤트가_할인이벤트_인지_판별한다() {
         //when
-        RewardMenu menuItem = event.wrapRewardMenu(null);
+        boolean result = discountEvent.isDiscountEvent();
         //then
-        Assertions.assertThat(menuItem).isEqualTo(rewardMenu);
+        assertThat(result).isTrue();
     }
 
-    static List<Arguments> provideEventData() {
-        return Arrays.asList(
-                //주말 할인 이벤트를 제외한 나머지 이벤트 참여 여부 결과를 true 를 예상한다.
-                Arguments.of(Event.of("크리스마스 디데이 할인", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 25), new ChristmasEventCondition()), true),
-                Arguments.of(Event.of("평일 할인", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 31), new WeekDayEventCondition()), true),
-                Arguments.of(Event.of("주말 할인", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 31), new WeekendEventCondition()), false),
-                Arguments.of(Event.of("특별 할인", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 31), new SpecialEventCondition()), true),
-                Arguments.of(Event.of("증정 이벤트", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 31), new GiftEventCondition()), true)
-        );
+    private EventRewardContext provideTestRewardContext() {
+        return new EventRewardContext(null, new CalendarDay(LocalDate.of(2023, 12, 25), true));
+    }
+
+    private EventConditionContext provideTestConditionContext(int day) {
+        return new EventConditionContext(10000, new CalendarDay(LocalDate.of(2023, 12, day), false));
+    }
+
+    private class TestDiscountEventCondition implements EventCondition {
+
+        @Override
+        public boolean canApplyEvent(EventConditionContext eventApplyParameter) {
+            return true;
+        }
+
+        @Override
+        public EventReward giveReward(EventRewardContext eventRewardParameter) {
+            return new EventReward(1000);
+        }
+
+        @Override
+        public EventType checkEventType() {
+            return EventType.DISCOUNT;
+        }
+    }
+
+
+    private class TestGiftEventCondition implements EventCondition {
+
+        @Override
+        public boolean canApplyEvent(EventConditionContext eventApplyParameter) {
+            return true;
+        }
+
+        @Override
+        public EventReward giveReward(EventRewardContext eventRewardParameter) {
+            return new EventReward(new RewardMenu(Menu.of("증정메뉴", 10000, MenuType.MAIN), 1));
+        }
+
+        @Override
+        public EventType checkEventType() {
+            return EventType.GIFT;
+        }
     }
 }
