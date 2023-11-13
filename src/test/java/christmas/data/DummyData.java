c072858ba4b448aa4dbeee373932b64cf88fd050
sinminseok12 <sinminseok12@naver.com>
2023-11-13 14:13:15 +0900

    test(CustomerTest): 구현된 고객 관련 기능 테스트 구현

--- src/test/java/christmas/data/DummyData.java
+++ src/test/java/christmas/data/DummyData.java
@@ -1,16 +1,32 @@
 package christmas.data;
 
 import christmas.domain.day.CalendarDay;
+import christmas.domain.event.Event;
 import christmas.domain.event.EventRewardContext;
+import christmas.domain.event.Events;
+import christmas.domain.event_condition.*;
 import christmas.domain.menu.Menu;
 import christmas.domain.menu.MenuItem;
 import christmas.domain.menu.MenuType;
 import christmas.domain.order.Order;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DummyData {
+
+    public static Events provideEventsData() {
+        List<Event> events = new ArrayList<>();
+
+        events.add(Event.of("크리스마스 디데이 할인", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 25), new ChristmasEventCondition()));
+        events.add(Event.of("평일 할인", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 31), new WeekDayEventCondition()));
+        events.add(Event.of("주말 할인", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 31), new WeekendEventCondition()));
+        events.add(Event.of("특별 할인", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 31), new SpecialEventCondition()));
+        events.add(Event.of("증정 이벤트", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 31), new GiftEventCondition()));
+
+        return new Events(events);
+    }
 
     public static Order provideOrderData() {
         Menu menu1 = Menu.of("메뉴1", 50000, MenuType.MAIN);
---
+++ src/test/java/christmas/domain/user/CustomerTest.java
@@ -0,0 +1,37 @@
+package christmas.domain.user;
+
+
+import christmas.domain.event.Reward;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static christmas.data.DummyData.*;
+
+public class CustomerTest {
+
+    private Customer customer;
+
+    @BeforeEach
+    void setInit() {
+        customer = new Customer(provideEventsData(), provideOrderData(), provideCalendarDay());
+    }
+
+    @Test
+    void findBeforeDiscountPrice_메서드는_할인전_주문총_금액을_찾는다() {
+        //when
+        Integer beforeDiscountPrice = customer.findBeforeDiscountPrice();
+        //then
+        Assertions.assertThat(beforeDiscountPrice).isEqualTo(300000);
+    }
+
+    @Test
+    void findAllReward_메서드는_적용된_모든_혜택_내역을_찾는다() {
+        //when
+        List<Reward> allReward = customer.findAllReward();
+        //then
+        Assertions.assertThat(allReward.size()).isEqualTo(5);
+    }
+}
