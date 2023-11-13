b20d7000c091d5a70ef00daaec1011b489f7583c
sinminseok12 <sinminseok12@naver.com>
2023-11-13 15:43:48 +0900

    feat(OutputView): 주문 메뉴 정보를 출력하는 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,11 +1,17 @@
 package christmas.view;
 
+import christmas.dto.MenuItemDto;
+
 import java.text.MessageFormat;
 import java.time.LocalDate;
+import java.util.List;
 
 public class OutputView {
+    private static final String NEXT_LINE = "\n";
     private static final String START_PLANNER_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String EVENT_PREVIEW_MESSAGE = "12월 {0}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private static final String ORDER_MENU_MESSAGE = NEXT_LINE + "<주문 메뉴>";
+    private static final String MENU_INFORMATION = "{0} {1}개";
 
     public static void printStartMessage() {
         System.out.println(START_PLANNER_MESSAGE);
@@ -13,5 +19,15 @@ public class OutputView {
 
     public static void printEventPreview(LocalDate localDate) {
         System.out.println(MessageFormat.format(EVENT_PREVIEW_MESSAGE, localDate.getDayOfMonth()));
+    }
+
+    public static void printOrderMenu(List<MenuItemDto> orderMenus) {
+        System.out.println(ORDER_MENU_MESSAGE);
+        orderMenus.stream()
+                .forEach(orderMenuDto -> System.out.println(formatMenu(orderMenuDto)));
+    }
+
+    private static String formatMenu(MenuItemDto orderMenu) {
+        return MessageFormat.format(MENU_INFORMATION, orderMenu.getMenuName(), orderMenu.getAmount());
     }
 }
