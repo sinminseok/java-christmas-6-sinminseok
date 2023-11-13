e04dcd328a5cd7dd8f4b65b35ba73b63589da657
sinminseok12 <sinminseok12@naver.com>
2023-11-13 15:46:00 +0900

    feat(OutputView): 할인 전 총 주문 금액을 출력하는 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,15 +3,19 @@ package christmas.view;
 import christmas.dto.MenuItemDto;
 
 import java.text.MessageFormat;
+import java.text.NumberFormat;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Locale;
 
 public class OutputView {
     private static final String NEXT_LINE = "\n";
     private static final String START_PLANNER_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String EVENT_PREVIEW_MESSAGE = "12월 {0}일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String ORDER_MENU_MESSAGE = NEXT_LINE + "<주문 메뉴>";
+    private static final String BEFORE_DISCOUNT_PRICE_MESSAGE = NEXT_LINE + "<할인 전 총주문 금액>";
     private static final String MENU_INFORMATION = "{0} {1}개";
+    private static final String PRICE_UNIT = "원";
 
     public static void printStartMessage() {
         System.out.println(START_PLANNER_MESSAGE);
@@ -25,6 +29,15 @@ public class OutputView {
         System.out.println(ORDER_MENU_MESSAGE);
         orderMenus.stream()
                 .forEach(orderMenuDto -> System.out.println(formatMenu(orderMenuDto)));
+    }
+
+    public static void printBeforeDiscountPrice(Integer price) {
+        System.out.println(BEFORE_DISCOUNT_PRICE_MESSAGE);
+        System.out.println(formatPrice(price) + PRICE_UNIT);
+    }
+
+    private static String formatPrice(int price) {
+        return NumberFormat.getNumberInstance(Locale.KOREA).format(price);
     }
 
     private static String formatMenu(MenuItemDto orderMenu) {
