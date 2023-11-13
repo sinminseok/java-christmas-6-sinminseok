9f4c56a477d8cd36fe571358fd1b297ded901da0
sinminseok12 <sinminseok12@naver.com>
2023-11-14 01:11:00 +0900

    refactor(OrderMenuTest): 테스트 클래스 이름 MenuItemTest -> OrderMenuTest 로 변경, 초기 데이터 재 설정

--- src/test/java/christmas/domain/menu/OrderMenuTest.java
+++ src/test/java/christmas/domain/menu/OrderMenuTest.java
@@ -7,21 +7,20 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class MenuItemTest {
+public class OrderMenuTest {
 
-    private OrderMenu menuItem;
+    private OrderMenu orderMenu;
 
     @BeforeEach
     void setInit() {
-        menuItem = new OrderMenu(Menu.of("메인", 10000, MenuType.MAIN), 1);
+        orderMenu = new OrderMenu(Menu.of("메인", 10000, MenuType.MAIN), 2);
     }
-
 
     @ParameterizedTest
     @CsvSource(value = {"MAIN, true", "APPETIZER, false", "DESERT, false", "DRINKING, false"})
     void compareMenuType_메서드는_메뉴타입을_비교해_동일성을_판별한다(MenuType menuType, boolean result) {
         //when
-        boolean sameMenuType = menuItem.compareMenuType(menuType);
+        boolean sameMenuType = orderMenu.compareMenuType(menuType);
         //then
         assertThat(sameMenuType).isEqualTo(result);
     }
@@ -29,7 +28,7 @@ public class MenuItemTest {
     @Test
     void calculatePrice_메서드는_메뉴의_가격과_수량을_곱한다() {
         //when
-        Integer price = menuItem.calculatePrice();
+        Integer price = orderMenu.calculatePrice();
         //then
         assertThat(price).isEqualTo(20000);
     }
