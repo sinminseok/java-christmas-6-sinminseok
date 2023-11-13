677fb998ce0906f271b24ee7bc6e18556a6ee3a7
sinminseok12 <sinminseok12@naver.com>
2023-11-13 19:46:15 +0900

    feat(Customer): 발급받은 배지 각각의 갯수를 구하는 기능 구현

--- src/main/java/christmas/domain/user/Customer.java
+++ src/main/java/christmas/domain/user/Customer.java
@@ -1,8 +1,13 @@
 package christmas.domain.user;
 
+import christmas.domain.plan.Badge;
 import christmas.domain.plan.Plan;
 
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Customer {
     private final List<Plan> plans;
@@ -13,5 +18,11 @@ public class Customer {
 
     public static Customer from(Plan plan) {
         return new Customer(List.of(plan));
+    }
+
+    public Map<Badge, Integer> findBadge() {
+        return plans.stream()
+                .map(Plan::awardBadge)
+                .collect(Collectors.toMap(Function.identity(), __ -> 1, Integer::sum, () -> new EnumMap<>(Badge.class)));
     }
 }
