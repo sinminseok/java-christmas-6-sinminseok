138b1df48484f53297614a110ed36f611a8779b1
sinminseok12 <sinminseok12@naver.com>
2023-11-13 15:03:29 +0900

    feat(RewardMenu): 증정 메뉴 이벤트 보상을 반환하기 위한 클래스 생성

---
+++ src/main/java/christmas/domain/event/RewardMenu.java
@@ -0,0 +1,28 @@
+package christmas.domain.event;
+
+import christmas.domain.menu.Menu;
+
+import java.util.Objects;
+
+public class RewardMenu {
+    private final Menu menu;
+    private final Integer count;
+
+    public RewardMenu(Menu menu, Integer count) {
+        this.menu = menu;
+        this.count = count;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RewardMenu that = (RewardMenu) o;
+        return Objects.equals(menu, that.menu) && Objects.equals(count, that.count);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(menu, count);
+    }
+}
