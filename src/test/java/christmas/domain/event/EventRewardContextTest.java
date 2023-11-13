4979219a176edd50f5441da99869a0fbbfea2f92
sinminseok12 <sinminseok12@naver.com>
2023-11-14 00:07:42 +0900

    refactor(EventRewardContextTest): 이벤트 보상을 계산하기 위한 테스트 클래스 독립적으로 실행되게 수정

--- src/test/java/christmas/domain/event/EventRewardContextTest.java
+++ src/test/java/christmas/domain/event/EventRewardContextTest.java
@@ -1,23 +1,41 @@
 package christmas.domain.event;
 
+import christmas.domain.day.CalendarDay;
+import christmas.domain.menu.Menu;
 import christmas.domain.menu.MenuType;
+import christmas.domain.menu.OrderMenu;
+import christmas.domain.order.Order;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import java.time.LocalDate;
+import java.util.List;
 
-import static christmas.data.DummyData.provideCalendarDay;
-import static christmas.data.DummyData.provideOrderData;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EventRewardContextTest {
+    private EventRewardContext eventRewardContext;
+
+    @BeforeEach
+    void setInit() {
+        Menu menu1 = Menu.of("메뉴1", 50000, MenuType.MAIN);
+        Menu menu2 = Menu.of("메뉴2", 50000, MenuType.MAIN);
+        Menu menu3 = Menu.of("메뉴3", 50000, MenuType.DESERT);
+
+        OrderMenu menuItem = new OrderMenu(menu1, 1);
+        OrderMenu menuItem2 = new OrderMenu(menu2, 1);
+        OrderMenu menuItem3 = new OrderMenu(menu3, 1);
+
+        Order order = new Order(List.of(menuItem, menuItem2, menuItem3));
+        CalendarDay calendarDay = new CalendarDay(LocalDate.of(2023, 12, 25), true);
+        eventRewardContext = new EventRewardContext(order, calendarDay);
+    }
 
     @ParameterizedTest
-    @CsvSource(value = {"MAIN, 3", "DESERT, 3"})
+    @CsvSource(value = {"MAIN, 2", "DESERT, 1"})
     void countMenuByType_메서드는_메뉴타입을_전달받아_주문에_있는_메뉴와_비교해_일치_갯수를_계산한다(MenuType menuType, int result) {
-        //given
-        EventRewardContext eventRewardContext = new EventRewardContext(provideOrderData(), provideCalendarDay());
         //when
         int count = eventRewardContext.countMenuByType(menuType);
         //then
@@ -25,15 +43,12 @@ public class EventRewardContextTest {
     }
 
     @Test
-    void calculateDaysSinceStart_메서드는_전잘받은_시작날짜와_종료날짜_사이의_일수를_계산한다() {
+    void calculateDaysSinceStart_메서드는_전달받은_시작날짜와_선택한_날짜_사이의_일수를_계산한다() {
         // given
         LocalDate startDate = LocalDate.of(2023, 12, 20);
-        // provideCalendarDay 날짜 : 2023.12.25
-        EventRewardContext eventRewardContext = new EventRewardContext(provideOrderData(), provideCalendarDay());
         // when
-        int daysSinceStart = eventRewardContext.calculateDaysSinceStart(startDate);
+        int daysSinceStart = eventRewardContext.calculateDaysBetweenStartAndGivenDate(startDate);
         // then
-
-        assertThat(daysSinceStart).isEqualTo(5); // Adjust the expected value based on the actual date difference
+        assertThat(daysSinceStart).isEqualTo(5);
     }
 }
