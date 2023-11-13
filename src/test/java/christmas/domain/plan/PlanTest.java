aa1d5498dae524dc1a4960cd22e4303aba31aee6
sinminseok12 <sinminseok12@naver.com>
2023-11-14 01:48:42 +0900

    refactor(PlanTest): 플랜 테스트 코드 독립적으로 실행되게 구현 및 calculatePaymentPrice, awardBadge 메서드 테스트 코드 구현

--- src/test/java/christmas/domain/plan/PlanTest.java
+++ src/test/java/christmas/domain/plan/PlanTest.java
@@ -1,14 +1,18 @@
 package christmas.domain.plan;
 
 
-import christmas.domain.event.Reward;
-import christmas.domain.event.RewardMenu;
+import christmas.domain.day.CalendarDay;
+import christmas.domain.event.*;
+import christmas.domain.menu.Menu;
+import christmas.domain.menu.MenuType;
+import christmas.domain.menu.OrderMenu;
+import christmas.domain.order.Order;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
-import static christmas.data.DummyData.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlanTest {
@@ -17,7 +21,7 @@ public class PlanTest {
 
     @BeforeEach
     void setInit() {
-        plan = new Plan(provideEventsData(), provideOrderData(), provideCalendarDay());
+        plan = new Plan(provideEvents(), provideOrder(), provideCalendarDay());
     }
 
     @Test
@@ -25,7 +29,7 @@ public class PlanTest {
         //when
         Integer beforeDiscountPrice = plan.findBeforeDiscountPrice();
         //then
-        assertThat(beforeDiscountPrice).isEqualTo(300000);
+        assertThat(beforeDiscountPrice).isEqualTo(50000);
     }
 
     @Test
@@ -41,7 +45,7 @@ public class PlanTest {
         //when
         List<RewardMenu> menuRewards = plan.findMenuRewards();
         //then
-        assertThat(menuRewards.size()).isEqualTo(1);
+        assertThat(menuRewards.size()).isEqualTo(2);
     }
 
     @Test
@@ -49,6 +53,40 @@ public class PlanTest {
         //when
         Integer rewardPrice = plan.calculateTotalRewardPrice();
         //then
-        assertThat(rewardPrice).isEqualTo(41538);
+        assertThat(rewardPrice).isEqualTo(23000);
+    }
+
+    @Test
+    void calculatePaymentPrice_메서드는_할인_후_예상_결제금액을_계산한다() {
+        //when
+        Integer paymentPrice = plan.calculatePaymentPrice();
+        //then
+        assertThat(paymentPrice).isEqualTo(47000);
+    }
+
+    @Test
+    void awardBadge_메서드는_총혜택_금액에_따라_이벤트_배지를_부여한다() {
+        Badge badge = plan.awardBadge();
+        //then
+        assertThat(badge).isEqualTo(Badge.SANTA);
+    }
+
+    private Events provideEvents() {
+        Event discountEvent1 = Event.of("테스트 할인 이벤트1", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 25), new EventTest.TestDiscountEventCondition());
+        Event discountEvent2 = Event.of("테스트 할인 이벤트2", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 25), new EventTest.TestDiscountEventCondition());
+        Event discountEvent3 = Event.of("테스트 할인 이벤트3", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 25), new EventTest.TestDiscountEventCondition());
+        Event giftEvent1 = Event.of("테스트 증정 이벤트1", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 25), new EventTest.TestGiftEventCondition());
+        Event giftEvent2 = Event.of("테스트 증정 이벤트2", LocalDate.of(2023, 12, 1), LocalDate.of(2023, 12, 25), new EventTest.TestGiftEventCondition());
+        return new Events(List.of(discountEvent1, discountEvent2, discountEvent3, giftEvent1, giftEvent2));
+    }
+
+    public static Order provideOrder() {
+        Menu menu = Menu.of("메뉴1", 50000, MenuType.MAIN);
+        OrderMenu menuItem = new OrderMenu(menu, 1);
+        return new Order(List.of(menuItem));
+    }
+
+    public static CalendarDay provideCalendarDay() {
+        return new CalendarDay(LocalDate.of(2023, 12, 25), true);
     }
 }
