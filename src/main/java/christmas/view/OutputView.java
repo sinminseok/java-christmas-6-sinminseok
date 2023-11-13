53bfe7fe64a5bca38d14986f0a10542d29746122
sinminseok12 <sinminseok12@naver.com>
2023-11-13 15:50:41 +0900

    feat(OutputView): 혜택 내역을 출력하는 기능 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,6 +1,7 @@
 package christmas.view;
 
 import christmas.dto.MenuItemDto;
+import christmas.dto.RewardDto;
 
 import java.text.MessageFormat;
 import java.text.NumberFormat;
@@ -15,7 +16,9 @@ public class OutputView {
     private static final String ORDER_MENU_MESSAGE = NEXT_LINE + "<주문 메뉴>";
     private static final String BEFORE_DISCOUNT_PRICE_MESSAGE = NEXT_LINE + "<할인 전 총주문 금액>";
     private static final String GIFT_REWARD_MESSAGE = NEXT_LINE + "<증정 메뉴>";
+    private static final String REWARD_MESSAGE = NEXT_LINE + "<혜택 내역>";
     private static final String MENU_INFORMATION = "{0} {1}개";
+    private static final String REWARD_INFORMATION = "{0}: -{1}원";
     private static final String PRICE_UNIT = "원";
     private static final String NONE = "없음";
     private static final Integer ZERO = 0;
@@ -49,11 +52,25 @@ public class OutputView {
                 .forEach(menuDto -> System.out.println(formatMenu(menuDto)));
     }
 
+    public static void printRewards(List<RewardDto> rewards) {
+        System.out.println(REWARD_MESSAGE);
+        if (rewards.size() == ZERO) {
+            System.out.println(NONE);
+            return;
+        }
+        rewards.stream()
+                .forEach(rewardDiscount -> System.out.println(formatReward(rewardDiscount)));
+    }
+
     private static String formatPrice(int price) {
         return NumberFormat.getNumberInstance(Locale.KOREA).format(price);
     }
 
     private static String formatMenu(MenuItemDto orderMenu) {
         return MessageFormat.format(MENU_INFORMATION, orderMenu.getMenuName(), orderMenu.getAmount());
+    }
+
+    private static String formatReward(RewardDto rewardDto) {
+        return MessageFormat.format(REWARD_INFORMATION, rewardDto.getName(), formatPrice(rewardDto.getDiscountPrice()));
     }
 }
