ebf435ce8df546109d48685b7f6c7107f7ab05fe
sinminseok12 <sinminseok12@naver.com>
2023-11-13 17:24:58 +0900

    feat(Customer): 고객 정보를 생성해 저장하는 기능 구현

---
+++ src/main/java/christmas/repository/CustomerRepository.java
@@ -0,0 +1,14 @@
+package christmas.repository;
+
+import christmas.domain.user.Customer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerRepository {
+    private static final List<Customer> customers = new ArrayList<>();
+
+    public static void save(Customer customer){
+        customers.add(customer);
+    }
+}
