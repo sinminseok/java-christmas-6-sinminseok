96885700825627145cf54602dcf6d0f0f94da9ea
sinminseok12 <sinminseok12@naver.com>
2023-11-14 14:19:05 +0900

    test(CalendarDayTest): 주말, 평일을 확인하는 기능 테스트 구현

--- src/test/java/christmas/domain/day/CalendarDayTest.java
+++ src/test/java/christmas/domain/day/CalendarDayTest.java
@@ -1,7 +1,10 @@
 package christmas.domain.day;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDate;
 
@@ -19,4 +22,28 @@ public class CalendarDayTest {
         //then
         assertThat(sameDay).isEqualTo(result);
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2})
+    void isWeekend_메서드는_주말날짜_인지_판별한다(int day){
+        //given
+        CalendarDay calendarDay = new CalendarDay(LocalDate.of(2023, 12, day), false);
+        //when
+        boolean weekend = calendarDay.isWeekend();
+        //then
+        Assertions.assertThat(weekend).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {3, 4, 5, 6, 7})
+    void isWeekDay_메서드는_평일날짜_인지_판별한다(int day){
+        //given
+        CalendarDay calendarDay = new CalendarDay(LocalDate.of(2023, 12, day), false);
+        //when
+        boolean weekend = calendarDay.isWeekDay();
+        //then
+        Assertions.assertThat(weekend).isTrue();
+    }
+
+
 }
